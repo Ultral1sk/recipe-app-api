9c2667663e8c67df7a64b39f56df7b25573c2086
ultral1sk <jderebanoski@gmail.com>
2024-07-23 11:50:51 +0200

    add token authentication for the user drf-token-auth

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,6 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+USER_SERIALIZER = 'user.serializers.UserSerializer'
+REGISTRATION_ENABLED = True
+
 SECRET_KEY = 'django-insecure-bqi-pv4i0_tubk4mcct3xj#m+)^4n44m!o5%ig4%ev0boc5uv7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -39,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'drf_spectacular',
     'rest_framework',
+    'rest_framework.authtoken',
     'core',
     'user'
 ]
@@ -135,5 +139,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
